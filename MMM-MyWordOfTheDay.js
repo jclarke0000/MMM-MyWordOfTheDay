30cfa4d4f7655ae5379412543cb3fdbb7549c8ed
Jeff Clarke <jeff@Jeffs-MacBook-Pro-M1.local>
2022-10-10 13:22:57 -0400

    Added hyphenation, example, and scrabble score. Switched to nunjucks templating

--- MMM-MyWordOfTheDay.js
+++ MMM-MyWordOfTheDay.js
@@ -16,6 +16,29 @@ Module.register("MMM-MyWordOfTheDay",{
     return ["MMM-MyWordOfTheDay.css"];
   },
 
+  getTemplate: function () {
+    return "mmm-my-word-of-the-day.njk";
+  },
+
+  /*
+    Data object provided to the Nunjucks template. The template does not
+    do any data minipulation; the strings provided here are displayed as-is.
+    The only logic in the template are conditional blocks that determine if
+    a certain section should be displayed, and simple loops for the hourly
+    and daily forecast.
+   */
+  getTemplateData: function () {
+    return {
+      phrases: {
+        loading: this.translate("LOADING")
+      },
+      loading: this.loading,
+      wordData: this.wordData,
+      scrabbleScore: this.wordData ? this.getScrabbleScore(this.wordData.word) : 0
+    };
+  },
+
+
   start: function() {
     this.loading = true;
     this.lastDate = null;
@@ -37,57 +60,43 @@ Module.register("MMM-MyWordOfTheDay",{
     }
   },
 
-  getDom: function() {
-
-    var wrapper = document.createElement("div");    
-
-    if (this.loading) {
-      var loading = document.createElement("div");
-      loading.innerHTML = this.translate("LOADING");
-      loading.className = "dimmed light small";
-      wrapper.appendChild(loading);
-      return wrapper;
+  getScrabbleScore: function(word) {
+    const pointsTable = {
+      "A" : 1,
+      "B" : 3,
+      "C" : 3,
+      "D" : 2,
+      "E" : 1,
+      "F" : 4,
+      "G" : 2,
+      "H" : 4,
+      "I" : 1,
+      "J" : 8,
+      "K" : 5,
+      "L" : 1,
+      "M" : 3,
+      "N" : 1,
+      "O" : 1,
+      "P" : 3,
+      "Q" : 10,
+      "R" : 1,
+      "S" : 1,
+      "T" : 1,
+      "U" : 1,
+      "V" : 4,
+      "W" : 4,
+      "X" : 8,
+      "Y" : 4,
+      "Z" : 10
     }
 
-    console.log(JSON.stringify(this.wordData));
-
-    var word = document.createElement("div");
-    word.classList.add("word");
-    word.innerHTML = this.wordData.word;
-    wrapper.appendChild(word);
-
-    if (this.wordData.pronounciation) {
-      var pronKey = document.createElement("div");
-      pronKey.classList.add("pronouciation");
-      pronKey.innerHTML = this.wordData.pronounciation;
-      wrapper.appendChild(pronKey);
-    } 
-
-    var definition = document.createElement("div");
-    definition.classList.add("definition");
-
-    var partOfSpeech = document.createElement("span");
-    partOfSpeech.classList.add("part-of-speech");
-    partOfSpeech.innerHTML = this.wordData.definition.partOfSpeech;
-    definition.appendChild(partOfSpeech);
-
-    var definitionText = document.createElement("span");
-    definitionText.classList.add("definition-text");
-    definitionText.innerHTML = this.wordData.definition.text;
-    definition.appendChild(definitionText);
-
-    wrapper.appendChild(definition);
-
-    if (this.wordData.note) {
-      var note = document.createElement("div");
-      note.classList.add("note");
-      note.innerHTML = this.wordData.note;
-      wrapper.appendChild(note);
+    var score = 0;
+    for (var i = 0; i < word.length; i++) {
+      score = score + pointsTable[word.charAt(i).toUpperCase()];
     }
 
-    return wrapper;
-
-  },
+    return score;
+  }
 
 
 });
--- node_helper.js
+++ node_helper.js
@@ -20,15 +20,17 @@ module.exports = NodeHelper.create({
     }
   },
 
-  getWord: async function(apiKey) {
+  getWord: function(apiKey) {
 
     var self = this;
-    var WoD_URL = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=" + apiKey;
+    // var WoD_URL = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=" + apiKey;
+    var WoD_URL = "https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=" + apiKey;
 
     axios.get(WoD_URL)
      .then( (response) => {
 
         var wordnikJSON = response.data;
+
         var wordData = new Object({
           word: wordnikJSON.word,
           definition: wordnikJSON.definitions[0], //just take the first definition
@@ -38,24 +40,75 @@ module.exports = NodeHelper.create({
           wordData.note = wordnikJSON.note;
         } 
 
-        //try and get pronunciation key
-        var pronounciationKeyURL = "http://api.wordnik.com:80/v4/word.json/" + wordnikJSON.word + "/pronunciations?useCanonical=false&limit=50&api_key=" + apiKey;
+        if (wordnikJSON.examples && wordnikJSON.examples[0]) {
+          wordData.example = wordnikJSON.examples[0].text;
+        } 
+
+
+
+
+        // try and get pronunciation key and hyphenation
+        var pronDone = false;
+        var hyphDone = false;
+
+        const pronounciationKeyURL = "https://api.wordnik.com/v4/word.json/" + wordnikJSON.word + "/pronunciations?api_key=" + apiKey;
 
         axios.get(pronounciationKeyURL)
-          .then( (response2) => {
+          .then( (r2)  => {
 
-            var pronKey = response2.data;
+            var pronKey = r2.data;
             if (pronKey.length > 0) {
               wordData.pronounciation = pronKey[0].raw;
             }
 
+
           })
-          .catch( (error2) => {
-            console.log("[MMM-MyWordOfTheDay] **ERROR getting Pronounciation Key ** : " + error2);
+          .catch( (e2) => {
+            //no pronunciation key available
+            wordData.pronounciation = "";
           })
           .finally( () => {
-            self.sendSocketNotification("WORD_UP", wordData);
+            pronDone = true;
+
+            if (hyphDone) {
+              self.sendSocketNotification("WORD_UP", wordData);              
+            }
+            
+          });
+
+        
+        const hyphenationURL = "https://api.wordnik.com/v4/word.json/" + wordnikJSON.word + "/hyphenation?api_key=" + apiKey;
+
+        axios.get(hyphenationURL)
+          .then( (r3)  => {
+
+            var hyphenationText = "";
+            r3.data.forEach( (syl) => {
+              if (hyphenationText !== "") {
+                hyphenationText = hyphenationText + "-";
+              }
+              if (syl.type && syl.type == "stress") {
+                hyphenationText = hyphenationText + syl.text.toUpperCase();
+              } else {
+                hyphenationText = hyphenationText + syl.text.toLowerCase();
+              }
+            });
+            wordData.hyphentation = hyphenationText;
+
+
           })
+          .catch( (e3) => {
+            //no hyphenation available
+            wordData.hyphentation = "";
+          })
+          .finally( () => {
+            hyphDone = true;
+
+            if (pronDone) {
+              self.sendSocketNotification("WORD_UP", wordData);              
+            }
+
+          });
 
       })
       .catch( (error) => {
